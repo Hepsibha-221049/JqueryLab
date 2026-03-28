c2a5c0cf49dbeea89d208a7b87e192500bdf0493
Hepsibha-221049 <n221049@rguktn.ac.in>
2026-03-28 18:44:11 +0530

    Addding Event Handling Methods

--- JqueryLab/script.js
+++ JqueryLab/script.js
@@ -1,45 +1,106 @@
+// $(document).ready(function () {
+//     $("#run").click(function () {
+//         $("p").css("color", "blue");
+//         $("#title").css('color', 'red');
+//         $(".text").css("background", "yellow");
+//         $("h2,p").css("border", "2px solid green");
+//     });
+
+//     $("div p").css({ "color": "purple", "background": "lightblue" });//selects all p inside div
+//     $("div > p").css("font-weight", "bold");//parent>child selects only direct child not deep
+//     $("h2+p").css("font-weight", "bold");//prev+next sibling
+//     $("h2~p").css("font-weight", "bold");//prev+next siblings(all)
+
+//     $("p:first").css("background", "pink");
+//     $("p:last").css("background", "orange");
+
+//     $("p:eq(2)").css("border", "3px solid red");
+//     $("li:even").css("background", "lightgreen");
+//     $("li:odd").css("background", "lightgray");
+//     $("p:not(.text)").css("color", "brown");
+
+
+//     $('[type]').css("border", "2px solid blue");
+//     $("[type='text']").css("background", "yellow");
+//     $("[name^='user']").css("color", "red");//starts with
+//     $("[name$='word']").css("background", "lightpink");//ends with
+//     $("[name*='mail']").css("border", "3px dashed green");//contains
+// });
+
+
+// $(document).ready(function () {
+//     $("#changeImg").click(function () {
+//         $("#myImg").attr("src", "../image2.jpg");
+//     });
+//     $("#disableCheck").click(function () {
+//         $("#check").prop("disabled", true);
+//     });
+//     $("#getValue").click(function () {
+//         alert("entered:" + $("#name").val());
+//     });
+//     $("#remove").click(function () {
+//         $("#myImg").removeAttr("width");
+//     });
+
+// });
+
 $(document).ready(function () {
-    $("#run").click(function () {
-        $("p").css("color", "blue");
-        $("#title").css('color', 'red');
-        $(".text").css("background", "yellow");
-        $("h2,p").css("border", "2px solid green");
+    $("#btn").click(function () {
+        alert("Button Clicked");
+    });
+    $("#dblBtn").dblclick(function () {
+        alert("Double Clicked");
+    });
+    $("#box").mouseenter(function () {
+        $(this).css("background", "lightblue");
+    });
+    $("#box").mouseleave(function () {
+        $(this).css("background", "lightgray");
+    });
+    $("#box").hover(
+        function () { $(this).css("border", "3px solid red"); },
+        function () { $(this).css("border", "none"); }
+    );
+    $("#box").mousedown(function () {
+        $(this).text("Mouse Down");
+    });
+    $("#box").mouseup(function () {
+        $(this).text("Mouse Up");
+    });
+    $("#keyboard").keypress(function () {
+        console.log("Key Pressed");
+    });
+    $("#keyboard").keydown(function () {
+        console.log("Key Down");
+    });
+    $("#keyboard").keyup(function () {
+        console.log("Key Up");
+    });
+    $("#myForm").submit(function (e) {
+        e.preventDefault(); // prevent page reload
+        alert("Form Submitted");
+    });
+    $("#course").change(function () {
+        alert("Selected: " + $(this).val());
     });
 
-    $("div p").css({ "color": "purple", "background": "lightblue" });//selects all p inside div
-    $("div > p").css("font-weight", "bold");//parent>child selects only direct child not deep
-    $("h2+p").css("font-weight", "bold");//prev+next sibling
-    $("h2~p").css("font-weight", "bold");//prev+next siblings(all)
-
-    $("p:first").css("background", "pink");
-    $("p:last").css("background", "orange");
-
-    $("p:eq(2)").css("border", "3px solid red");
-    $("li:even").css("background", "lightgreen");
-    $("li:odd").css("background", "lightgray");
-    $("p:not(.text)").css("color", "brown");
-
-
-    $('[type]').css("border", "2px solid blue");
-    $("[type='text']").css("background", "yellow");
-    $("[name^='user']").css("color", "red");//starts with 
-    $("[name$='word']").css("background", "lightpink");//ends with 
-    $("[name*='mail']").css("border", "3px dashed green");//contains
-});
-
-
-$(document).ready(function () {
-    $("#changeImg").click(function () {
-        $("#myImg").attr("src", "../image2.jpg");
+    // focus
+    $("#name").focus(function () {
+        $(this).css("background", "lightyellow");
     });
-    $("#disableCheck").click(function () {
-        $("#check").prop("disabled", true);
+
+    // blur
+    $("#name").blur(function () {
+        $(this).css("background", "white");
     });
-    $("#getValue").click(function () {
-        alert("entered:" + $("#name").val());
+    $("#btn").on("mouseover", function () {
+        $(this).css("background", "green");
     });
-    $("#remove").click(function () {
-        $("#myImg").removeAttr("width");
+
+    // off (remove event)
+    $("#removeEvent").click(function () {
+        $("#btn").off("click");
+        alert("Click event removed");
     });
 
 });
